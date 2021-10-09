9327c7efaf0180cdef4748f7ba7d03a6f9697fbf
Jet <67730137+jetapps1@users.noreply.github.com>
2021-10-09 11:56:29 +1100

    Fixed formatting
    - Fixed the SQL formatting

--- RFIDdbSQL.sql
+++ RFIDdbSQL.sql
@@ -1,34 +1,34 @@
 CREATE TABLE Store (
-    StoreID int NOT NULL,
-    Name varchar(255) NOT NULL,
-    Address varchar(255) NOT NULL,
-    PRIMARY KEY (StoreID),
+	StoreID int NOT NULL,
+	Name varchar(255) NOT NULL,
+	Address varchar(255) NOT NULL,
+	PRIMARY KEY (StoreID),
 );
 
 CREATE TABLE Customer (
-    CustomerID int NOT NULL,
-    Name varchar(255) NOT NULL,
-    PhoneNum varchar(255) NOT NULL,
-    COVIDStatus varchar(255) NOT NULL,
-    PRIMARY KEY (CustomerID),
+	CustomerID int NOT NULL,
+	Name varchar(255) NOT NULL,
+	PhoneNum varchar(255) NOT NULL,
+	COVIDStatus varchar(255) NOT NULL,
+	PRIMARY KEY (CustomerID),
 );
 
 CREATE TABLE Checkin (
-    CheckinID int NOT NULL,
-    StoreID int NOT NULL,
-    CustomerID int NOT NULL,
-    Date varchar(255) NOT NULL,
-    Time varchar(255) NOT NULL,
-    PRIMARY KEY (CheckinID),
-    FOREIGN KEY (StoreID) REFERENCES Store(StoreID),
-    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+	CheckinID int NOT NULL,
+	StoreID int NOT NULL,
+	CustomerID int NOT NULL,
+	Date varchar(255) NOT NULL,
+	Time varchar(255) NOT NULL,
+	PRIMARY KEY (CheckinID),
+	FOREIGN KEY (StoreID) REFERENCES Store(StoreID),
+	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
 
 CREATE TABLE Dashboard (
-    CheckinID int NOT NULL,
-    Username int NOT NULL,
-    Password int NOT NULL,
-    FOREIGN KEY (CheckinID) REFERENCES Checkin(CheckinID)
+	CheckinID int NOT NULL,
+	Username int NOT NULL,
+	Password int NOT NULL,
+	FOREIGN KEY (CheckinID) REFERENCES Checkin(CheckinID)
 );
 
 CREATE TABLE user (
@@ -57,7 +57,7 @@ CREATE TABLE item (
 );
 
 CREATE TABLE Tag (
-  Tag_ID_number varchar(50) DEFAULT NULL,
-  Time_recorded datetime DEFAULT NULL,
-  Information varchar(20) DEFAULT NULL
+	Tag_ID_number varchar(50) DEFAULT NULL,
+	Time_recorded datetime DEFAULT NULL,
+	Information varchar(20) DEFAULT NULL
 );
